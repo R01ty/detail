c3c4b8224d238e7d0f274439a3feb8f868bbb4ec
R01ty <ragulrock360@gmail.com>
2024-07-09 16:04:49 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import './App.css';
+
+const App = () => {
+  const [showDetails, setShowDetails] = useState(false);
+
+  const toggleDetails = () => {
+    setShowDetails(!showDetails);
+  };
+
+  return (
+    <div className="container">
+      <div className="card">
+        <div className="icon">
+        <img src="20240309_094903.jpg" alt="eye" />
+        </div>
+        <h2 className="name">Name: Ragul</h2>
+        <p className="registration">Registration No: 212221043005</p>
+        <button className="button" onClick={toggleDetails}>
+          {showDetails ? 'Hide Details' : 'Show Details'}
+        </button>
+        {showDetails && (
+          <div className="details">
+            Additional details or content can go here...
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default App;
